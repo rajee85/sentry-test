073c9bb5f0f50f6abbcef9b522a64019d9dfd057
rajee85 <79933273+rajee85@users.noreply.github.com>
2021-03-28 10:14:05 -0400

    Create python-version.py

---
+++ python-version.py
@@ -0,0 +1,42 @@
+import sys
+major,minor,patch = 0,0,0
+
+with open('version','r') as f:
+    major,minor,patch = f.readline().split(".")
+
+#print(major,minor,patch)
+
+# take user input
+inp = sys.argv[1]
+
+def switch_func(inp):
+    global major, minor, patch
+    if inp == 'major':
+        major = int(major)+1
+        minor = 0
+        patch =0
+
+        return(str(major)+'.'+str(minor)+'.'+str(patch))
+
+    elif inp == 'minor':
+        major = int(major)
+        minor = int(minor)+1
+        patch = 0
+
+        return(str(major)+'.'+str(minor)+'.'+str(patch))
+
+    elif inp == 'patch':
+        major = int(major)
+        minor = int(minor)
+        patch = int(patch)+1
+
+        return(str(major)+'.'+str(minor)+'.'+str(patch))
+
+    else:
+        major = int(major)
+        minor = int(minor)
+        patch = int(patch)
+
+        return(str(major)+'.'+str(minor)+'.'+str(patch))
+
+print(switch_func(inp))
